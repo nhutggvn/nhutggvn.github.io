7a6187c5823773fb3cb27504d42819928de84be8
installapp <nhutgg.paypal.97@gmail.com>
2023-11-06 15:29:38 +0700

    update html

--- js1/script.js
+++
@@ -1,8 +0,0 @@
-function buyProduct(productName) {
-    // Thực hiện các hành động liên quan đến việc mua sản phẩm, ví dụ: chuyển hướng đến trang thanh toán.
-    // Dựa vào productName để xác định sản phẩm cụ thể.
-    if (productName === 'youtube') {
-        window.location.href = 'https://t.me/nhutnguyenminh2';
-    }
-    // Thêm các sản phẩm khác ở đây
-}
